424747d44769d03e787ff759dc0118ff2dac7bf5
joshharris0 <joshharri@hartford.edu>
2025-11-12 17:05:42 -0500

    started matrix
    still need to finish flipMatrix method

--- matrix/Matrix.java
+++ matrix/Matrix.java
@@ -1,7 +1,47 @@
 public class Matrix {
-  public static void main (String[] args){
-    for (int a = 0, b = input - 1; a!=b; a++, b--){
-      System.out.println(array[a,b]);
+  private int[][] matrix;
+  private int size;
+
+  public Matrix(int size){
+    this.matrix = new int[size][size];
+    this.size = size;
+    System.out.println("Created a matrix with dimensions " + size + "x" + size);
+  }
+
+  public void populateMatrix() {
+    int index = 1;
+    for (int i = 0; i < size; i++) {
+      for (int j = 0; j < size; j++) {
+        matrix[i][j] = index++;
+        // matrix[i][j] = 0;
+      }
     }
   }
+
+  public void printMatrix() {
+    final String YELLOW = "\u001b[33m";
+    final String NORMAL = "\u001b[0m";
+
+    for (int i = 0; i < size; i++) {
+      for (int j = 0; j < size; j++) {
+        if (j == (size - 1) - i) {
+          System.out.print(YELLOW + matrix[i][j] + NORMAL + "\t");
+        } else {
+          System.out.print(matrix[i][j] + "\t");
+        }
+      }
+      System.out.println();
+    }
+  }
+
+  private void swap(int x1, int y1, int x2, int y2) {
+    int temp = matrix[x1][y1];
+    matrix[x1][y1] = matrix[x2][y2];
+    matrix[x2][y2] = temp;
+  }
+
+  public void flipMatrix() {
+  }
+
+
 }
--- matrix/RunMatrix.java
+++ matrix/RunMatrix.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
-public class RunMatrix {
+public class RunMatrix{
   public static void main(String[] args) {
+    Matrix matrix = new Matrix(6);
+    matrix.populateMatrix();
+    matrix.printMatrix();
   }
 }
